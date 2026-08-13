7334ab21e572a8032e2287487b9adce10d0345c7
majidhussainqadri1-dot <majidhussainqadri1@gmail.com>
2026-08-13 07:09:27 +0500

    File 03 review R16: contain structured-data provider failures

--- includes/class-spd-frontend.php
+++ includes/class-spd-frontend.php
@@ -9,6 +9,7 @@ final class SPD_Frontend {
 	use SPD_Frontend_Future;
 	use SPD_Frontend_Report, SPD_Frontend_Central {
 		SPD_Frontend_Central::structured_data insteadof SPD_Frontend_Report;
+		SPD_Frontend_Central::structured_data as private central_structured_data;
 		SPD_Frontend_Report::structured_data as legacy_structured_data;
 	}
 
@@ -29,5 +30,18 @@ final class SPD_Frontend {
 		add_action( 'admin_post_spd_report_profile', array( $this, 'report' ) );
 		add_action( 'admin_post_nopriv_spd_report_profile', array( $this, 'reject_anonymous' ) );
 		add_action( 'wp_head', array( $this, 'structured_data' ), 20 );
+	}
+
+	public function structured_data() {
+		try {
+			$this->central_structured_data();
+		} catch ( Throwable $exception ) {
+			do_action( 'sabri_file24_profile_provider_failure', array(
+				'owner' => 'file03',
+				'surface' => 'frontend_structured_data',
+				'exception_class' => sanitize_key( get_class( $exception ) ),
+				'at' => SPD_Helpers::now(),
+			) );
+		}
 	}
 }
